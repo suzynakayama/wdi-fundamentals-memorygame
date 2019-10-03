7073ca8cbf7845773eeb36ff69c6c90fed07c600
Suzy Nakayama <suzynakayama@Suzys-MacBook-Pro.local>
2019-10-03 18:04:12 -0400

    More game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -5,24 +5,24 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log(cardsInPlay);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry, try again.");
-    }
+    } 
 }
 
-/*Write */
+function flipCard(cardId) {
+    if (cardsInPlay.length === 2) {
+   checkForMatch();
+}
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+
+
+/*Comment*/
